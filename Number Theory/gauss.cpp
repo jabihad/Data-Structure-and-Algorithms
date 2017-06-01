c92ca42a5c9c6a947c32514c1faa4c06db4c64da
Abu Bakar Siddique <jabihad@gmail.com>
2017-06-01 21:25:30 +0600

    Update gauss.cpp

--- Number Theory/gauss.cpp
+++ Number Theory/gauss.cpp
@@ -7,15 +7,17 @@ int main()
 {
     int i,j,k,n,id,x,y;
     char ch;
+    printf("Please Enter the number of equation: ");
     scanf("%d",&n);
     for(i=0; i<n; i++)
     {
+        printf("Equation %d : ",i+1);
         for(j=0; j<n+1; j++)
         {
             scanf("%lf",&mat[i][j]);
         }
+        printf("\n");
     }
-    printf("\n\n");
     for(i=0; i<n; i++)
     {
         id = i;
@@ -35,6 +37,7 @@ int main()
                     mat[j][k] -= factor*mat[i][k];
             }
     }
+    printf("Solution is :\n");
     for(i=0,j=3,ch='x',x=0,y=0; i<n; i++,ch++,x++,y++)
     {
         printf("%c = %.0lf\n",ch,mat[i][j]/mat[x][y]);
